c914f4fe9962f5d9d66d7023ff3934cdf724e60c
Christian <christianlindberg01@hotmail.com>
2025-01-29 17:48:32 +0100

    conc added

--- BE/src/config/db.js
+++ BE/src/config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// Delete this later // see readme in FE
+const mongoURI =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/turid';
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(mongoURI),
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection failed:', error.message);
