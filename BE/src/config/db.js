7c073c14429bf5d7148f38598374ad619a3207ff
MapYh <145336421+MapYh@users.noreply.github.com>
2025-01-30 20:05:41 +0100

    Added the normal mongodb url.

--- BE/src/config/db.js
+++ BE/src/config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.Mongodb_url);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
