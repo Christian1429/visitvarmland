556a2b8d31c5d109ccbb56f2651a1e20b6496541
Christian <christianlindberg01@hotmail.com>
2025-02-01 17:42:48 +0100

    date and timepicker functions done

--- FE/src/pages/Form.jsx
+++ FE/src/pages/Form.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import {
   Box,
   Typography,
   Breadcrumbs,
   Link,
   useMediaQuery,
-  Button,
 } from "@mui/material";
+import './Form.css';
+import { useTheme } from '@mui/material/styles';
 import Contact from "../components/Contact";
 import ClientNew from "../components/ClientNew";
 import ClientEvent from "../components/ClientEvent";
+import ClientExist from '../components/ClientExist';
 import BtnNext from "../components/Buttons/BtnNext";
 import CloseBtn from "../components/Buttons/CloseBtn";
-import { useTheme } from "@mui/material/styles";
-import "./Form.css";
-import ClientExist from "../components/ClientExist";
 import { FormDataContext } from "../context/FormDataContext";
-import handleSubmit from "../utils/handleSubmit";
 import { handleChange, handleArrayChange } from "../utils/formUtils";
+import handleSubmit from '../utils/handleSubmit';
+import DatePickerClient from "../components/DatePicker";
 
 const Form = () => {
   const theme = useTheme();
@@ -56,6 +56,7 @@ const Form = () => {
       case 2:
         return (
           <>
+            <DatePickerClient formData={formData} setFormData={setFormData} />
             <ClientEvent
               formData={formData}
               setFormData={setFormData}
