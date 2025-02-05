3573499c8300185ab5589d04a38dbd381e22ac1a
MapYh <145336421+MapYh@users.noreply.github.com>
2025-02-05 14:28:26 +0100

    Reverted commits.

--- BE/src/config/db.js
+++ BE/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
-const mongoURI = process.env.MongoAtlas;
+const mongoURI = process.env.MONGODB_URIs;
 
 const connectDB = async () => {
   try {
